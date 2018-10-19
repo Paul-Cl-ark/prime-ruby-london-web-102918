c7e5edbbb4f03a66a0c552ebfc6e61258ee3fb5d
Paul Clark <clark.paul123@gmail.com>
2018-10-19 11:37:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -12,7 +12,7 @@ end
 
 def prime?(integer)
     if integer >= 2 
-      (2..integer - 1).each { |i| return false if (integer % i == 0 }
+      (2..integer - 1).each { |i| return false if (integer % i == 0) }
     end
   else true
 end

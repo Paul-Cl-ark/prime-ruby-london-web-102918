07a4f140e504a95045d38c1e37e767f840f8edfc
Paul Clark <clark.paul123@gmail.com>
2018-10-19 11:44:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,13 +8,23 @@
   true
 end
 
-=end
-
 def prime?(integer)
   if integer >= 2 
       (2..integer - 1).each { |i| integer % i != 0 }
       true
   else 
     false
+  end
+end
+
+=end 
+
+def prime?(num)
+  if num >= 2
+    (2...num).all? do |x|
+      num % x != 0
+    end
+  else
+    return false
   end
 end
